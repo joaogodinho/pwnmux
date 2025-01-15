49705b0006d8b6f99630c48481aab4667b859017
joaogodinho <joaogodinho@users.noreply.github.com>
2025-01-15 18:10:10 +0000

    Feat: Use pwndbg.config instead of gdb.execute

--- gdbinit.py
+++ gdbinit.py
@@ -1,7 +1,7 @@
 import os
 import atexit
 
-import gdb
+import pwndbg
 from pwndbg.commands.context import contextoutput
 
 panes = {
@@ -26,12 +26,12 @@ contextoutput("expressions", panes['regs'][1], True, 'top', False)
 
 # To see more options to customize run `theme` and `config` in gdb
 # Increase the amount of lines shown in disasm and stack
-gdb.execute("set context-disasm-lines 25")
-gdb.execute("set context-stack-lines 18")
+pwndbg.config.context_disasm_lines.value = 25
+pwndbg.config.context_stack_lines.value = "18"
 # Give backtrace a little more color
-gdb.execute("set backtrace-prefix-color red,bold")
-gdb.execute("set backtrace-address-color gray")
-gdb.execute("set backtrace-symbol-color red")
-gdb.execute("set backtrace-frame-label-color green")
+pwndbg.config.backtrace_prefix_color.value = "red,bold"
+pwndbg.config.backtrace_address_color.value = "gray"
+pwndbg.config.backtrace_symbol_color.value = "red"
+pwndbg.config.backtrace_frame_label_color.value = "green"
 # Remove the panes when gdb is exited
 atexit.register(lambda: [os.popen(F"tmux kill-pane -t {p[0]}").read() for p in panes.values()])
